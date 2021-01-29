3f2d339eac37cd1a37c57fc7bfa116a2332bbe05
net234 <51489394+net234@users.noreply.github.com>
2021-01-29 21:22:28 +0100

    Acces method to get and set actives offsets
    void MPU6050::getActiveOffsets(int16_t Data[6]);
    void MPU6050::setActiveOffsets(int16_t offsets[6]);

--- src/MPU6050.h
+++ src/MPU6050.h
@@ -827,7 +827,8 @@ class MPU6050 {
 		void CalibrateAccel(uint8_t Loops = 15);// Fine tune after setting offsets with less Loops.
 		void PID(uint8_t ReadAddress, float kP,float kI, uint8_t Loops);  // Does the math
 		void PrintActiveOffsets(); // See the results of the Calibration
-
+		void getActiveOffsets(int16_t offsets[6]); // Get the results of the Calibration  
+		void setActiveOffsets(int16_t offsets[6]); // Set offsets
 
 
         // special methods for MotionApps 2.0 implementation
